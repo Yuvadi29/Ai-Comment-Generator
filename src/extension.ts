3294ee5eaf787e2979125de3386ae138654a17eb
Yuvadi29 <adityatrivedi612@gmail.com>
2025-01-11 19:51:10 +0530

    Updating the Activate Extension Section

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('commentgenerator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from commentGenerator!');
+	const generateCommentCommand = vscode.commands.registerCommand('commentgenerator.generateComment', async () => {
+		vscode.window.showInformationMessage('Generating comment, please wait!!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateCommentCommand);
+
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
